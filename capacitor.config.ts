40bda23ac071038a4777e12cec80a189c9ba3b1c
Atlonoficial <geral1atlontech@gmail.com>
2025-09-13 22:36:54 -0300

    capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.atlon.shapepro',
+  appId: 'com.atlontech.shapepro.aluno',
   appName: 'Shape Pro',
   webDir: 'dist',
   bundledWebRuntime: false,
